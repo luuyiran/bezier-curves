0c275871372a310e056e5d74fa594af2bdc02baa
luuyiran <luuyiran@gmail.com>
2021-11-21 01:33:55 +0800

    fixed precision

--- bezier.cpp
+++ bezier.cpp
@@ -4,26 +4,26 @@
 // 作　　者：luuyiran@gmail.com
 // 发布日期：2021-11-18
 //
-#pragma warning(disable:4996)// _stprintf
-#define _USE_MATH_DEFINES	// M_PI_2
-#include <math.h>			// sin
-#include <stdint.h>			// uint64_t
-#include <vector>			// array
-#include <easyx.h>			// window
+#include <math.h>				// sin
+#include <stdint.h>				// uint64_t
+#include <vector>				// array
+#include <easyx.h>				// window
 
 using std::vector;
 
 #define	WIDTH			800		// 宽
 #define	HEIGHT			600		// 高
 
-// 初始化控制点
-vector<POINT> controlPoints;
+struct Point { double x, y; };
 
-POINT operator+(const POINT& a, const POINT& b) {
-	return POINT({ a.x + b.x, a.y + b.y });
+// 初始化控制点
+vector<Point> controlPoints;
+
+Point operator+(const Point& a, const Point& b) {
+	return Point({ a.x + b.x, a.y + b.y });
 }
-POINT operator*(double f, const POINT& p) {
-	return POINT({(LONG)(f*p.x), (LONG)(f*p.y)});
+Point operator*(double f, const Point& p) {
+	return Point({f*p.x, f*p.y});
 }
 
 // 计算二项式系数： C(n, k) = n! / (k!(n-k)!)
@@ -42,27 +42,30 @@ int C(int n, int k) {
 }
 
 // 绘制贝塞尔曲线，绘制 t [0 - end_t] 范围
-void drawBezier(const vector<POINT>& points, double end_t) {
+void drawBezier(const vector<Point>& points, double end_t) {
 	if (points.size() <= 1) return;
 	int n = points.size() - 1; // 阶次为点数 - 1
+	setfillcolor(RED);
 	for (double t = 0.0; t <= end_t; t += 0.001) {
-		POINT p{ 0 };
+		Point p{ 0 };
 		for (int k = 0; k <= n; k++)
 			p = p + C(n, k) * pow(t, k) * pow(1 - t, (double)n - k) * points[k];
-		setfillcolor(RED);
-		solidcircle(p.x, p.y, 3);
+		// 位置四舍五入
+		p.x += 0.5;
+		p.y += 0.5;
+		solidcircle((int)p.x, (int)p.y, 3);
 	}
 }
 
 // 递归获取每一层的控制点
-void bezierLevelPoints(vector<vector<POINT>>& levels, double t) {
-	vector<POINT> &pre = levels.back();
-	vector<POINT> next;
+void bezierLevelPoints(vector<vector<Point>>& levels, double t) {
+	vector<Point> &pre = levels.back();
+	vector<Point> next;
 	int n = pre.size();
 	if (n <= 1) return;
 	
 	for (int i = 0; i < n - 1; i++) {
-		POINT point = (1 - t) * pre[i] + t * pre[i + 1];
+		Point point = (1 - t) * pre[i] + t * pre[i + 1];
 		next.push_back(point);
 	}
 	levels.push_back(next);
@@ -76,8 +79,8 @@ void processInput() {
 		// 按住左键拖动控制点
 		if (WM_MOUSEMOVE == msg.message && msg.lbutton) {
 			for (auto& p : controlPoints) {
-				int dx = p.x - msg.x;
-				int dy = p.y - msg.y;
+				int dx = (int)p.x - msg.x;
+				int dy = (int)p.y - msg.y;
 				if (dx * dx + dy * dy < 50) {
 					p.x = msg.x;
 					p.y = msg.y;
@@ -86,7 +89,7 @@ void processInput() {
 		}
 		// 点击右键，创建控制点
 		else if (WM_RBUTTONDOWN == msg.message && msg.rbutton) {
-			controlPoints.push_back({ msg.x, msg.y });
+			controlPoints.push_back({ (double)msg.x, (double)msg.y });
 		}
 		// 按空格键清空
 		else if (WM_KEYDOWN == msg.message && msg.vkcode == VK_SPACE) {
@@ -96,26 +99,27 @@ void processInput() {
 }
 
 // 画点、画线
-void drawControlLines(vector<vector<POINT>>& levels) {
+void drawControlLines(vector<vector<Point>>& levels) {
 	int k = levels.size();
 	COLORREF color[8] = { YELLOW, BROWN, CYAN, LIGHTRED, WHITE, GREEN, MAGENTA, RED };
 	for (int i = 0; i < k; i++) {
-		vector<POINT> cur = levels[i];
+		vector<Point> cur = levels[i];
 		int n = cur.size();
 		setlinecolor(color[i&0XF]);
 		setfillcolor(color[i&0XF]);
 		for (auto& point : cur)
-			solidcircle(point.x, point.y, 4);
+			solidcircle((int)(point.x + 0.5), (int)(point.y + 0.5), 3);
 		if (k <= 2) continue;
 		for (int j = 0; j < n - 1; j++)
-			line(cur[j].x, cur[j].y, cur[j + 1].x, cur[j + 1].y);
+			line((int)(cur[j].x + 0.5), (int)(cur[j].y + 0.5), \
+				(int)(cur[j + 1].x + 0.5), (int)(cur[j + 1].y + 0.5));
 	}
 }
 
 int main() {
 	TCHAR  buf[32];
 	double t = 0.f;
-	double time = -M_PI_2;
+	double time = -1.57079632679489661923;   // pi / 2;
 	double deltaTime = 0.f; // 当前帧和上一帧的时间差
 	double lastTime = 0.f;
 	double currentTime = 0.f;
@@ -138,22 +142,22 @@ int main() {
 		// 根据鼠标拖动更新控制点
 		processInput();
 		// 得到每一层的控制点
-		vector<vector<POINT>> levels({ controlPoints });
+		vector<vector<Point>> levels({ controlPoints });
 		bezierLevelPoints(levels, t);
 		// 绘制控制点、控制线
 		drawControlLines(levels);
 		// 绘制 0 - t 范围内的贝塞尔曲线
 		drawBezier(controlPoints, t);
 
-		// 操作提示
+		// 提示信息
 		outtextxy(0,  0, L"  Right Mouse Button to Create Points.");
 		outtextxy(0, 16, L"  Left  Mouse Button to Drag Points.");
 		outtextxy(0, 32, L"  Press SPACE to Clear.");
-		_stprintf(buf, _T("  t: %.3f"), t);
+		swprintf_s(buf, _T("  t: %.3f"), t);
 		outtextxy(0, 48, buf);
-		_stprintf(buf, _T("  time: %.0fms"), 1000 * deltaTime);
+		swprintf_s(buf, _T("  time: %.0fms"), 1000 * deltaTime);
 		outtextxy(0, 64, buf);
-		_stprintf(buf, _T("  fps: %d\n"), (int)(1.f / deltaTime));
+		swprintf_s(buf, _T("  fps: %d\n"), (int)(1.f / deltaTime));
 		outtextxy(0, 80, buf);
         FlushBatchDraw();
     }
